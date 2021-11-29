aa21d3b785455760da44d98426f72349f9156f0c
Oleksii Veselskyi <nightinzond@gmail.com>
2021-11-29 19:41:12 +0200

    Datagridviews configured

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,47 +29,122 @@ namespace Qualifying_work
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dgvColDesc = new System.Windows.Forms.DataGridView();
+            this.buttonDownload = new System.Windows.Forms.Button();
+            this.buttonReady = new System.Windows.Forms.Button();
+            this.dgvRowDesc = new System.Windows.Forms.DataGridView();
+            this.dgvMain = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvColDesc)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRowDesc)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dgvColDesc
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(132, 12);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
-            this.dataGridView1.TabIndex = 0;
+            this.dgvColDesc.AllowUserToAddRows = false;
+            this.dgvColDesc.AllowUserToDeleteRows = false;
+            this.dgvColDesc.AllowUserToResizeColumns = false;
+            this.dgvColDesc.AllowUserToResizeRows = false;
+            this.dgvColDesc.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgvColDesc.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvColDesc.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvColDesc.ColumnHeadersVisible = false;
+            this.dgvColDesc.Location = new System.Drawing.Point(105, 12);
+            this.dgvColDesc.MultiSelect = false;
+            this.dgvColDesc.Name = "dgvColDesc";
+            this.dgvColDesc.ReadOnly = true;
+            this.dgvColDesc.RowHeadersVisible = false;
+            this.dgvColDesc.RowTemplate.Height = 25;
+            this.dgvColDesc.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dgvColDesc.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dgvColDesc.Size = new System.Drawing.Size(196, 71);
+            this.dgvColDesc.TabIndex = 0;
             // 
-            // button1
+            // buttonDownload
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonDownload.Location = new System.Drawing.Point(12, 12);
+            this.buttonDownload.Name = "buttonDownload";
+            this.buttonDownload.Size = new System.Drawing.Size(87, 23);
+            this.buttonDownload.TabIndex = 1;
+            this.buttonDownload.Text = "Завантажити";
+            this.buttonDownload.UseVisualStyleBackColor = true;
+            // 
+            // buttonReady
+            // 
+            this.buttonReady.Location = new System.Drawing.Point(12, 41);
+            this.buttonReady.Name = "buttonReady";
+            this.buttonReady.Size = new System.Drawing.Size(87, 23);
+            this.buttonReady.TabIndex = 2;
+            this.buttonReady.Text = "Готово!";
+            this.buttonReady.UseVisualStyleBackColor = true;
+            // 
+            // dgvRowDesc
+            // 
+            this.dgvRowDesc.AllowUserToAddRows = false;
+            this.dgvRowDesc.AllowUserToDeleteRows = false;
+            this.dgvRowDesc.AllowUserToResizeColumns = false;
+            this.dgvRowDesc.AllowUserToResizeRows = false;
+            this.dgvRowDesc.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgvRowDesc.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvRowDesc.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRowDesc.ColumnHeadersVisible = false;
+            this.dgvRowDesc.Location = new System.Drawing.Point(105, 98);
+            this.dgvRowDesc.MultiSelect = false;
+            this.dgvRowDesc.Name = "dgvRowDesc";
+            this.dgvRowDesc.ReadOnly = true;
+            this.dgvRowDesc.RowHeadersVisible = false;
+            this.dgvRowDesc.RowTemplate.Height = 25;
+            this.dgvRowDesc.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dgvRowDesc.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dgvRowDesc.Size = new System.Drawing.Size(35, 27);
+            this.dgvRowDesc.TabIndex = 3;
+            // 
+            // dgvMain
+            // 
+            this.dgvMain.AllowUserToAddRows = false;
+            this.dgvMain.AllowUserToDeleteRows = false;
+            this.dgvMain.AllowUserToResizeColumns = false;
+            this.dgvMain.AllowUserToResizeRows = false;
+            this.dgvMain.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgvMain.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMain.ColumnHeadersVisible = false;
+            this.dgvMain.Location = new System.Drawing.Point(375, 12);
+            this.dgvMain.MultiSelect = false;
+            this.dgvMain.Name = "dgvMain";
+            this.dgvMain.RowHeadersVisible = false;
+            this.dgvMain.RowTemplate.Height = 25;
+            this.dgvMain.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dgvMain.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dgvMain.Size = new System.Drawing.Size(46, 23);
+            this.dgvMain.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgvMain);
+            this.Controls.Add(this.dgvRowDesc);
+            this.Controls.Add(this.buttonReady);
+            this.Controls.Add(this.buttonDownload);
+            this.Controls.Add(this.dgvColDesc);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvColDesc)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRowDesc)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dgvColDesc;
+        private System.Windows.Forms.Button buttonDownload;
+        private System.Windows.Forms.Button buttonReady;
+        private System.Windows.Forms.DataGridView dgvRowDesc;
+        private System.Windows.Forms.DataGridView dgvMain;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,10 +12,158 @@ namespace Qualifying_work
 {
     public partial class Form1 : Form
     {
+        const int cellSize = 25;
         public Form1()
         {
             InitializeComponent();
+            ResizeDgvs(2, 2, 5, 5);
         }
 
+        private void ResizeDgvs(int maxRowB, int maxColB, int newCC, int newRC)
+        {
+            for (int i = 0; i < newCC; i++)
+            {
+                dgvColDesc.Columns.Add(i.ToString(), i.ToString());
+                dgvColDesc.Columns[i].Width = cellSize;
+                dgvMain.Columns.Add(i.ToString(), i.ToString());
+                dgvMain.Columns[i].Width = cellSize;
+            }
+            for (int i = 0; i < maxColB; i++)
+            {
+                dgvColDesc.Rows.Add();
+            }
+            for (int i = 0; i < maxRowB; i++)
+            {
+                dgvRowDesc.Columns.Add(i.ToString(), i.ToString());
+                dgvRowDesc.Columns[i].Width = cellSize;
+            }
+            for (int i = 0; i < newRC; i++)
+            {
+                dgvRowDesc.Rows.Add();
+                dgvMain.Rows.Add();
+            }
+            dgvColDesc.Width = newCC * cellSize + 4;
+            dgvRowDesc.Width = maxRowB * cellSize + 4;
+            dgvMain.Width = newCC * cellSize + 4;
+            
+            dgvColDesc.Height = maxColB * cellSize + 4;
+            dgvRowDesc.Height = newRC * cellSize + 4;
+            dgvMain.Height = newRC * cellSize + 4;
+
+            dgvRowDesc.Top = dgvColDesc.Top + dgvColDesc.Height + 5;
+            dgvColDesc.Left = dgvRowDesc.Left + dgvRowDesc.Width + 5;
+            dgvMain.Left = dgvColDesc.Left;
+            dgvMain.Top = dgvRowDesc.Top;
+        }
+
+        private void InitializeDgvs()
+        {
+            //if (main.ColumnCount > 0)
+            //{
+            //    for (int i = main.ColumnCount - 1; i >= 0; i--)
+            //    {
+            //        main.Columns.RemoveAt(i);
+            //        add.Columns.RemoveAt(i);
+            //    }
+            //}
+           
+            //if (main.RowCount > 0)
+            //{
+            //    for (int i = main.RowCount - 1; i >= 0; i--)
+            //    {
+            //        main.Rows.RemoveAt(i);
+            //        add.Rows.RemoveAt(i);
+            //    }
+            //}
+        }
+
+        /*private void ChangeTableColCount(DataGridView dgvColDesc, DataGridView dgvMain, int newCount)
+        {
+            if (newCount < 2)
+            {
+                throw new Exception("Кількість стовпців не може бути меншою за 1.");
+            }
+            int oldCount = main.ColumnCount;
+            for (int i = oldCount; i < newCount; i++)
+            {
+                main.Columns.Add(i.ToString(), i.ToString());
+                main.Columns[i].Width = 50;
+                main[i, 0].Value = s + (i - 1);
+                main[i, 0].ReadOnly = true;
+                add.Columns.Add(i.ToString(), i.ToString());
+                add.Columns[i].Width = 50;
+            }
+            for (int i = 0; i < oldCount - newCount; i++)
+            {
+                main.Columns.RemoveAt(oldCount - i - 1);
+                add.Columns.RemoveAt(oldCount - i - 1);
+            }
+            main.Width = newCount * 50 + 4;
+            add.Width = newCount * 50 + 4;
+        }
+
+        private void ChangeTableRowCount(DataGridView main, DataGridView add, int newCount, string s, bool isInput)
+        {
+            if (newCount < 2)
+            {
+                throw new Exception("Кількість рядків не може бути меншою за 1.");
+            }
+            int oldCount;
+            if (automaton is MealyAut)
+            {
+                oldCount = add.RowCount;
+                if (isInput)
+                {
+                    for (int i = oldCount; i < newCount - 1; i++)
+                    {
+                        add.Rows.Add();
+                        add[0, i].Value = s + i;
+                        add[0, i].ReadOnly = true;
+                    }
+                }
+                else
+                {
+                    for (int i = oldCount - 1; i < newCount - 1; i++)
+                    {
+                        add.Rows.Add();
+                        add[0, i].Value = addTableIn[0, i].Value;
+                    }
+                    add.Rows.RemoveAt(add.RowCount - 1);
+                }
+                for (int i = 0; i < oldCount - newCount + 1; i++)
+                {
+                    add.Rows.RemoveAt(oldCount - i - 1);
+                }
+                add.Height = (newCount - 1) * 22 + 4;
+            }
+            oldCount = main.RowCount;
+            for (int i = oldCount; i < newCount; i++)
+            {
+                main.Rows.Add();
+                if (isInput)
+                {
+                    main[0, i].Value = s + (i - 1);
+                }
+                else
+                {
+                    main[0, i].Value = mainTableIn[0, i].Value;
+                }
+            }
+            for (int i = 0; i < oldCount - newCount; i++)
+            {
+                main.Rows.RemoveAt(oldCount - i - 1);
+            }
+            main.Height = newCount * 22 + 4;
+            if (automaton is MooreAut)
+            {
+                add.Top = 48;
+                main.Top = add.Top + add.Height + 5;
+            }
+            else
+            {
+                main.Top = 48;
+                add.Top = main.Top + main.Height + 5;
+            }
+        }*/
     }
 }
