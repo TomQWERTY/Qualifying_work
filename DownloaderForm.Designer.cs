c8d014f9ac3760bee79ad7901aea1f309667fc7d
Oleksii Veselskyi <nightinzond@gmail.com>
2022-05-29 20:47:21 +0300

    Ability to download random nonogram added

--- DownloaderForm.Designer.cs
+++ DownloaderForm.Designer.cs
@@ -32,60 +32,88 @@
             this.radioButtonID = new System.Windows.Forms.RadioButton();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.buttonDownload = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.labelLoad = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // radioButtonRand
             // 
             this.radioButtonRand.AutoSize = true;
-            this.radioButtonRand.Location = new System.Drawing.Point(12, 12);
+            this.radioButtonRand.Location = new System.Drawing.Point(12, 25);
             this.radioButtonRand.Name = "radioButtonRand";
-            this.radioButtonRand.Size = new System.Drawing.Size(111, 17);
+            this.radioButtonRand.Size = new System.Drawing.Size(232, 17);
             this.radioButtonRand.TabIndex = 0;
-            this.radioButtonRand.Text = "Download random";
+            this.radioButtonRand.Text = "Випадковий (оберіть кількість клітинок): ";
             this.radioButtonRand.UseVisualStyleBackColor = true;
             // 
             // radioButtonID
             // 
             this.radioButtonID.AutoSize = true;
             this.radioButtonID.Checked = true;
-            this.radioButtonID.Location = new System.Drawing.Point(12, 78);
+            this.radioButtonID.Location = new System.Drawing.Point(12, 48);
             this.radioButtonID.Name = "radioButtonID";
-            this.radioButtonID.Size = new System.Drawing.Size(101, 17);
+            this.radioButtonID.Size = new System.Drawing.Size(52, 17);
             this.radioButtonID.TabIndex = 1;
             this.radioButtonID.TabStop = true;
-            this.radioButtonID.Text = "Donwload by ID";
+            this.radioButtonID.Text = "За ID";
             this.radioButtonID.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 101);
+            this.textBox1.Location = new System.Drawing.Point(70, 47);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(33, 20);
             this.textBox1.TabIndex = 2;
             this.textBox1.Text = "3";
             // 
             // buttonDownload
             // 
-            this.buttonDownload.Location = new System.Drawing.Point(12, 127);
+            this.buttonDownload.Location = new System.Drawing.Point(12, 71);
             this.buttonDownload.Name = "buttonDownload";
-            this.buttonDownload.Size = new System.Drawing.Size(75, 23);
+            this.buttonDownload.Size = new System.Drawing.Size(91, 23);
             this.buttonDownload.TabIndex = 3;
-            this.buttonDownload.Text = "Download";
+            this.buttonDownload.Text = "Завантажити";
             this.buttonDownload.UseVisualStyleBackColor = true;
             this.buttonDownload.Click += new System.EventHandler(this.buttonDownload_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "<40",
+            "40-100",
+            "100-200",
+            "200-400",
+            ">400"});
+            this.comboBox1.Location = new System.Drawing.Point(250, 24);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(61, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // labelLoad
+            // 
+            this.labelLoad.AutoSize = true;
+            this.labelLoad.Location = new System.Drawing.Point(12, 9);
+            this.labelLoad.Name = "labelLoad";
+            this.labelLoad.Size = new System.Drawing.Size(77, 13);
+            this.labelLoad.TabIndex = 5;
+            this.labelLoad.Text = "Завантажити:";
             // 
             // DownloaderForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(366, 231);
+            this.ClientSize = new System.Drawing.Size(329, 106);
+            this.Controls.Add(this.labelLoad);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.buttonDownload);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.radioButtonID);
             this.Controls.Add(this.radioButtonRand);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "DownloaderForm";
-            this.Text = "Download  a nonogram";
+            this.Text = "Завантаження кросворду";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -97,5 +125,7 @@
         private System.Windows.Forms.RadioButton radioButtonID;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button buttonDownload;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label labelLoad;
     }
 }
--- DownloaderForm.cs
+++ DownloaderForm.cs
@@ -18,29 +18,67 @@ namespace Qualifying_work
         {
             f1 = f1_;
             InitializeComponent();
+            comboBox1.SelectedIndex = 0;
         }
 
         private void buttonDownload_Click(object sender, EventArgs e)
         {
-            f1.npg.StartWork();
-            DataTable res = f1.npg.Query("select blocks_descriptions, need_backtracking, verified from nonograms where id=" + textBox1.Text);
-            if (res.Rows.Count < 1)
+            int indInRes = 0;
+            bool allOk = true;
+            DataTable res;
+            if (radioButtonID.Checked)
             {
-                MessageBox.Show("Помилка! Кросворду з таким Id не існує або він був видалений.");
-            }
-            else if (!Convert.ToBoolean(res.Rows[0][2]))
-            {
-                MessageBox.Show("Помилка! Даний кросворд ще не був перевірений адміністратором.");
+                f1.npg.StartWork();
+                res = f1.npg.Query("select blocks_descriptions, need_backtracking, verified from nonograms where id=" + textBox1.Text);
+                f1.npg.FinishWork();
+                if (res.Rows.Count < 1)
+                {
+                    MessageBox.Show("Помилка! Кросворду з таким Id не існує або він був видалений.");
+                    allOk = false;
+                }
+                else if (!Convert.ToBoolean(res.Rows[0][2]))
+                {
+                    MessageBox.Show("Помилка! Даний кросворд ще не був перевірений адміністратором.");
+                    allOk = false;
+                }
             }
             else
             {
-                var temp_ = res.Rows[0].ItemArray[0];
+                List<string> sizeStr = new List<string>(comboBox1.SelectedItem.ToString().Split('-'));
+                if (sizeStr[0][0] == '<')
+                {
+                    sizeStr.Add(sizeStr[0]);
+                    sizeStr[0] = "0";
+                    sizeStr[1] = sizeStr[1].Substring(1);
+                }
+                else if (sizeStr[0][0] == '>')
+                {
+                    sizeStr[0] = sizeStr[0].Substring(1);
+                    sizeStr.Add("1000000");
+                }
+                f1.npg.StartWork();
+                res = f1.npg.Query("select blocks_descriptions, need_backtracking, verified from nonograms where " +
+                    "blocks_descriptions[1]*blocks_descriptions[2]>" + sizeStr[0] + " and blocks_descriptions[1]*blocks_descriptions[2]<" + sizeStr[1]);
+                f1.npg.FinishWork();
+                if (res.Rows.Count < 1)
+                {
+                    MessageBox.Show("Помилка! Наразі на жаль немає кросвордів з обраними розмірами.");
+                    allOk = false;
+                }
+                else
+                {
+                    Random rnd = new Random();
+                    indInRes = rnd.Next(0, res.Rows.Count);
+                }
+            }
+            if (allOk)
+            {
+                var temp_ = res.Rows[indInRes].ItemArray[0];
                 int[] blocksDescs = (int[])temp_;
                 f1.ses = new NonogramToSolveSession(new Nonogram(blocksDescs, Convert.ToInt32(textBox1.Text)));
                 f1.ses.NGram.Type = Convert.ToBoolean(res.Rows[0][1]) ? NonogramType.NeedBacktracking : NonogramType.OnlyILL;
                 this.DialogResult = DialogResult.OK;
             }
-            f1.npg.FinishWork();
         }
     }
 }
